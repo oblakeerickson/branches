5992ea8a7d9b0de300e096b023aa215445fbd960
Blake Erickson <o.blakeerickson@gmail.com>
2014-11-20 06:00:23 -0700

    moved puts statement to bin directory, this way you can include this gem in another project and it will return a list of branches without echoing out

--- lib/branches.rb
+++ lib/branches.rb
@@ -3,7 +3,7 @@ require "branches/version"
 module Branches
   class Commands
     def show_branches_by_date
-      puts %x[git for-each-ref --sort=-committerdate refs/heads/ --format='%(committerdate:short) %(refname:short)']
+      %x[git for-each-ref --sort=-committerdate refs/heads/ --format='%(committerdate:short) %(refname:short)']
     end
   end
 end
